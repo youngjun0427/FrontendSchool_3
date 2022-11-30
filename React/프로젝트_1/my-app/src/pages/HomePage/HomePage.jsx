c92ff866ff37fc115aa3bbe0666c5620fbae64bc
youngjun.choi <dlfgudwns12@gmail.com>
2022-11-30 16:39:41 +0900

    TDD

--- React/프로젝트_1/my-app/src/pages/HomePage/HomePage.jsx
+++ React/프로젝트_1/my-app/src/pages/HomePage/HomePage.jsx
@@ -1,19 +1,48 @@
+import { useState, useEffect, useContext } from 'react'
+
 import Card from '../../components/Card/Card'
 import Cart from '../../components/Cart/Cart'
 import FailLoadData from '../../components/Shared/FailLoadData/FailLoadData'
-import { data } from '../../database/data'
+
+// import { data } from '../../database/data'
+import { callAPIResponse } from '../../database/callAPI'
 import './homePage.css'
+
+console.log('진입점 1')
 
 export default function HomePage() {
     // console.log(data)
     // const data = null
+    const [loadData, setLoadData] = useState(null)
+    console.log('진입점 2')
+
+    // useEffect(()=>{
+    //     console.log('로딩중...')
+    // }, [])
+
+    // useEffect(()=>{
+    //     if(loadData === null) {
+    //         return
+    //     } else {
+    //         console.log('로딩완료!!')
+    //         console.log(loadData)
+    //     }
+    // }, [loadData])
+
+    useContext(callAPIResponse).then(data => {
+        console.log('진입점 3')
+        console.log(data)
+        setLoadData(data)
+    })
+    
     return (
         <main className='product'>
         {
-            data !== null ? 
+            loadData !== null ? 
                 <>
                     <ul className='product-list'>
-                        {data.map(item =><Card key={item.id} {...item}/>)}
+                        {loadData.map(item =><Card key={item.id} {...item}/>)}
+                        {console.log('진입점 4')}
                     </ul>
                     <Cart/>
                 </>
@@ -22,3 +51,4 @@ export default function HomePage() {
         </main>
     )
 }
+
